483b8787053250552c6fa11f3a98e0a4bc627b4d
Marsll <Marsll@users.noreply.github.com>
2019-09-28 17:22:37 +0200

    Add header over both pages

---
+++ application/dashapp/alt_index_string.py
@@ -0,0 +1,36 @@
+html_layout = '''<!DOCTYPE html>
+                    <html>
+                        <head>
+                            {%metas%}
+                            <title>{%title%}</title>
+                            {%favicon%}
+                            {%css%}
+                        </head>
+                        <body>
+                            <div class="header">
+                                <nav class="navbar navbar-expand-md navbar-dark bg-dark">
+                                    <div class = "container">
+                                    <div class="navbar-collapse collapse w-100 order-1 order-md-0 dual-collapse2">
+                                        <a class="navbar-brand" href="/">
+                                        <img src="../static/img/header_logo.png" width="30" height="30" class="d-inline-block align-top" alt="">
+                                        Explore Your Tinder
+                                        </a>
+                                    </div>
+                                    <div class="navbar-collapse collapse w-100 order-3 dual-collapse2">
+                                        <ul class="navbar-nav ml-auto">
+                                            <li class="nav-item">
+                                                <a class="nav-link" href="/about">About</a>
+                                            </li>
+                                        </ul>
+                                    </div>
+                                    </div>  
+                                </nav>
+                            </div>
+                            {%app_entry%}
+                            <footer>
+                                {%config%}
+                                {%scripts%}
+                                {%renderer%}
+                            </footer>
+                        </body>
+                    </html>'''
---
+++ application/dashapp/card.py
@@ -0,0 +1,11 @@
+import dash_html_components as html
+import dash_core_components as dcc
+
+# card component
+def generate_card(description, value):
+    card = html.Div([
+        html.Div([],
+        className)
+    ],           
+    className="card"
+)
